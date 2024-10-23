31981aa5f04de948f41f40b0c716cb9a7b774591
userA <geniusssbg@gmail.com>
2024-10-24 00:56:38 +0800

    <Auto-Frontend> 上线时间:2024-10-24 00:53:38

--- genius-blog-front/src/api/BlogController.js
+++ genius-blog-front/src/api/BlogController.js
@@ -1,41 +1,36 @@
-import request from '@/utils/request';
-
-
-export function uploadImg(img ) {
-     return request({
-        url: '/app/v1/blog/uploadImg/',
-        method: 'post',
-        data: {
-            img:img,
-        }
-     });
+import request from "@/utils/request";
+export function addBlog(blogDTO) {
+    return request({
+        url: "/app/v1/blog/addBlog/",
+        method: "post",
+        data: blogDTO,
+    });
 }
-export function addBlog(blogDTO ) {
-     return request({
-        url: '/app/v1/blog/addBlog/',
-        method: 'post',
-        data: {
-            blogDTO:blogDTO,
-        }
-     });
+export function uploadImg(img) {
+    const formData = new FormData();
+    formData.append("img", img);
+    return request({
+        url: "/app/v1/blog/uploadImg/",
+        method: "post",
+        data: formData,
+    });
 }
-export function oneBlog(blogId ) {
-     return request({
-        url: '/app/v1/blog/oneBlog/',
-        method: 'get',
+export function oneBlog(blogId) {
+    return request({
+        url: "/app/v1/blog/oneBlog",
+        method: "get",
         params: {
-            blogId:blogId,
-        }
-     });
+            blog_id: blogId,
+        },
+    });
 }
-export function blogPageList(page, limit ) {
-     return request({
-        url: '/app/v1/blog/blogPageList/',
-        method: 'get',
+export function blogPageList(page, limit) {
+    return request({
+        url: "/app/v1/blog/blogPageList/",
+        method: "get",
         params: {
-            page:page,
-            limit:limit,
-        }
-     });
+            page: page,
+            limit: limit,
+        },
+    });
 }
-
--- genius-blog-front/src/api/BlogTagController.js
+++ genius-blog-front/src/api/BlogTagController.js
@@ -1,21 +1,15 @@
 import request from '@/utils/request';
-
-
-export function addTag(blogTagDTO ) {
-     return request({
+export function addTag(blogTagDTO) {
+    return request({
         url: '/app/v1/tag/addTag/',
         method: 'post',
-        data: {
-            blogTagDTO:blogTagDTO,
-        }
-     });
+        data: blogTagDTO,
+    });
 }
 export function tagList() {
-     return request({
+    return request({
         url: '/app/v1/tag/tags/',
         method: 'get',
-        params: {
-        }
-     });
+        params: {}
+    });
 }
-
